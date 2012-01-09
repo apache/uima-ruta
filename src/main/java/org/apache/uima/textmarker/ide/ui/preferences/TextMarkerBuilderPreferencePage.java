721296112cc4d8b9278af6446924a78b37317739
Peter Klügl <pkluegl@apache.org>
2012-01-09 14:11:11 +0000

    UIMA-2322 was not able to find a solution and therefore replaced the complete page by a simpler version. Also deactivated TaskTag preferences.
    git-svn-id: https://svn.apache.org/repos/asf/uima/sandbox/trunk/TextMarker/uimaj-ep-textmarker-ide@1229161 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/uima/textmarker/ide/ui/preferences/TextMarkerBuilderPreferencePage.java
+++ src/main/java/org/apache/uima/textmarker/ide/ui/preferences/TextMarkerBuilderPreferencePage.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -20,52 +20,45 @@
 package org.apache.uima.textmarker.ide.ui.preferences;
 
 import org.apache.uima.textmarker.ide.TextMarkerIdePlugin;
-import org.apache.uima.textmarker.ide.core.TextMarkerNature;
-import org.eclipse.dltk.ui.preferences.AbstractConfigurationBlockPreferencePage;
-import org.eclipse.dltk.ui.preferences.IPreferenceConfigurationBlock;
-import org.eclipse.dltk.ui.preferences.OverlayPreferenceStore;
-import org.eclipse.jface.util.IPropertyChangeListener;
-import org.eclipse.jface.util.PropertyChangeEvent;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
+import org.apache.uima.textmarker.ide.core.TextMarkerCorePreferences;
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.FieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
 
-public class TextMarkerBuilderPreferencePage extends AbstractConfigurationBlockPreferencePage {
-  @Override
-  protected String getHelpId() {
+/**
+ * Preference page to manage preferences for the ide plugin.
+ */
+public class TextMarkerBuilderPreferencePage extends FieldEditorPreferencePage
+        implements IWorkbenchPreferencePage {
 
-    return null;
-  }
-
-  @Override
-  protected void setDescription() {
+  private BooleanFieldEditor builderImport;
+  private FieldEditor builderResolve;
+  private BooleanFieldEditor builderShortNames;
+  
+  public TextMarkerBuilderPreferencePage() {
+    setPreferenceStore(TextMarkerIdePlugin.getDefault().getPreferenceStore());
     setDescription("Builder");
   }
 
   @Override
-  protected void setPreferenceStore() {
-    setPreferenceStore(TextMarkerIdePlugin.getDefault().getPreferenceStore());
+  protected void createFieldEditors() {
+    builderResolve = new BooleanFieldEditor(TextMarkerCorePreferences.BUILDER_RESOLVE_IMPORTS,
+            TextMarkerPreferencesMessages.BuilderResolveImports, getFieldEditorParent());
+    addField(builderResolve);
+    
+    builderImport = new BooleanFieldEditor(TextMarkerCorePreferences.BUILDER_IMPORT_BY_NAME,
+            TextMarkerPreferencesMessages.BuilderImportByName, getFieldEditorParent());
+    addField(builderImport);
+    
+    builderShortNames = new BooleanFieldEditor(TextMarkerCorePreferences.BUILDER_IGNORE_DUPLICATE_SHORTNAMES,
+            TextMarkerPreferencesMessages.BuilderIgnoreDuplicateShortnames, getFieldEditorParent());
+    addField(builderShortNames);
   }
 
-  @Override
-  protected Label createDescriptionLabel(Composite parent) {
-    return null;
+  public void init(IWorkbench workbench) {
   }
-
-  @Override
-  protected IPreferenceConfigurationBlock createConfigurationBlock(
-          OverlayPreferenceStore overlayPreferenceStore) {
-    overlayPreferenceStore.addPropertyChangeListener(new IPropertyChangeListener() {
-
-      public void propertyChange(PropertyChangeEvent event) {
-        String property = event.getProperty();
-        Object newValue = event.getNewValue();
-        if (newValue instanceof Boolean) {
-          TextMarkerIdePlugin.getDefault().getPluginPreferences()
-                  .setValue(property, (Boolean) newValue);
-        }
-      }
-    });
-    return new TextMarkerBuilderConfigurationBlock(this, overlayPreferenceStore,
-            TextMarkerNature.NATURE_ID);
-  }
+  
+ 
 }
