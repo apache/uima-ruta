49983bf5c5f6a3ed2ea873c75a0947564fd77d8e
Peter Klügl <pkluegl@apache.org>
2013-07-01 12:25:21 +0000

    UIMA-3027 - added command to convert projects
    git-svn-id: https://svn.apache.org/repos/asf/uima/sandbox/ruta/trunk@1498408 13f79535-47bb-0310-9956-ffa450edef68

--- ruta-ep-addons/src/main/java/org/apache/uima/ruta/utils/update/ConvertProjectHandler.java
+++ ruta-ep-addons/src/main/java/org/apache/uima/ruta/utils/update/ConvertProjectHandler.java
@@ -19,25 +19,18 @@
 
 package org.apache.uima.ruta.utils.update;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
 import org.apache.uima.ruta.addons.RutaAddonsPlugin;
 import org.apache.uima.ruta.ide.core.RutaNature;
-import org.apache.uima.ruta.ide.core.builder.RutaProjectUtils;
 import org.apache.uima.ruta.ide.ui.wizards.RutaProjectCreationWizard;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.commands.IHandlerListener;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
-import org.eclipse.core.resources.IProjectNature;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
@@ -45,18 +38,19 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.dltk.core.DLTKCore;
 import org.eclipse.dltk.core.IScriptProject;
+import org.eclipse.dltk.utils.ResourceUtil;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-public class UpdateProjectHandler implements IHandler {
+public class ConvertProjectHandler implements IHandler {
 
-  private class ConverterHandlerJob extends Job {
+  private class ConvertProjectHandlerJob extends Job {
     ExecutionEvent event;
 
-    ConverterHandlerJob(ExecutionEvent event) {
-      super("Updating...");
+    ConvertProjectHandlerJob(ExecutionEvent event) {
+      super("Converting...");
       this.event = event;
       setUser(true);
     }
@@ -78,71 +72,31 @@ public class UpdateProjectHandler implements IHandler {
           }
         }
       }
-      monitor.beginTask("Updating UIMA Ruta project...", projects.size());
+      monitor.beginTask("Converting to UIMA Ruta project...", projects.size());
       for (IProject each : projects) {
-        // update old projects
         try {
-          IProjectDescription description = each.getDescription();
-          String[] natureIds = description.getNatureIds();
-          int counter = 0;
-          boolean oldProject = false;
-          for (String id : Arrays.asList(natureIds)) {
-            if (id.equals("org.apache.uima.textmarker.ide.nature")) {
-              natureIds[counter] = RutaNature.NATURE_ID;
-              oldProject = true;
-            }
-            counter++;
-          }
-          if (oldProject) {
-            description.setNatureIds(natureIds);
-            each.setDescription(description, monitor);
-            List<File> files = getFiles(new File(each.getLocation().toPortableString()));
-            for (File file : files) {
-              String absolutePath = file.getAbsolutePath();
-              if (file.getName().endsWith(".tm")) {
-                File newFile = new File(absolutePath.substring(0, absolutePath.length() - 3)
-                        + ".ruta");
-                file.renameTo(newFile);
-              }
-            }
-            IScriptProject sp = DLTKCore.create(each);
-            List<IFolder> scriptFolders = RutaProjectUtils.getScriptFolders(sp);
-            for (IFolder iFolder : scriptFolders) {
-              iFolder.refreshLocal(IResource.DEPTH_INFINITE, monitor);
-            }
-          }
+          IScriptProject scriptProject = DLTKCore.create(each);
+          ResourceUtil.addNature(each, monitor, RutaNature.NATURE_ID);
+          RutaProjectCreationWizard.createRutaProject(scriptProject, null, monitor);
         } catch (CoreException e) {
           RutaAddonsPlugin.error(e);
         }
-
-        try {
-          IProjectNature nature = each.getNature(RutaNature.NATURE_ID);
-          if (nature != null) {
-            List<IFolder> descriptorFolders = RutaProjectUtils.getDescriptorFolders(each);
-            if (descriptorFolders != null && !descriptorFolders.isEmpty()) {
-              IFolder descFolder = descriptorFolders.get(0);
-              RutaProjectCreationWizard.copyDescriptors(descFolder);
-              descFolder.refreshLocal(IResource.DEPTH_INFINITE, monitor);
-            }
-          }
-        } catch (CoreException e) {
-          RutaAddonsPlugin.error(e);
-        }
-        monitor.worked(1);
       }
       monitor.done();
       return Status.OK_STATUS;
     }
   }
 
-  public void addHandlerListener(IHandlerListener handlerListener) {
+  public void addHandlerListener(IHandlerListener arg0) {
+
   }
 
   public void dispose() {
+
   }
 
   public Object execute(ExecutionEvent event) throws ExecutionException {
-    new ConverterHandlerJob(event).schedule();
+    new ConvertProjectHandlerJob(event).schedule();
     return Status.OK_STATUS;
   }
 
@@ -154,24 +108,8 @@ public class UpdateProjectHandler implements IHandler {
     return true;
   }
 
-  public void removeHandlerListener(IHandlerListener handlerListener) {
+  public void removeHandlerListener(IHandlerListener arg0) {
 
-  }
-
-  private static List<File> getFiles(File dir) {
-    List<File> result = new ArrayList<File>();
-    for (File each : dir.listFiles()) {
-      if (each.isHidden()) {
-        continue;
-      }
-      if (each.getName().endsWith(".tm")) {
-        result.add(each);
-      }
-      if (each.isDirectory()) {
-        result.addAll(getFiles(each));
-      }
-    }
-    return result;
   }
 
 }
