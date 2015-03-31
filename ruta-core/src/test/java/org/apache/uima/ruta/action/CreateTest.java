1a1085be0ea2f9a220951c743b883bb57cbe9cb9
Peter Klügl <pkluegl@apache.org>
2015-03-31 20:23:33 +0000

    UIMA-4319 - added test
    git-svn-id: https://svn.apache.org/repos/asf/uima/ruta/trunk@1670465 13f79535-47bb-0310-9956-ffa450edef68

--- ruta-core/src/test/java/org/apache/uima/ruta/action/CreateTest.java
+++ ruta-core/src/test/java/org/apache/uima/ruta/action/CreateTest.java
@@ -20,6 +20,7 @@
 package org.apache.uima.ruta.action;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,9 +30,11 @@ import java.util.TreeMap;
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.FSIterator;
 import org.apache.uima.cas.Feature;
+import org.apache.uima.cas.FeatureStructure;
 import org.apache.uima.cas.Type;
 import org.apache.uima.cas.text.AnnotationFS;
 import org.apache.uima.cas.text.AnnotationIndex;
+import org.apache.uima.ruta.engine.Ruta;
 import org.apache.uima.ruta.engine.RutaEngine;
 import org.apache.uima.ruta.engine.RutaTestUtils;
 import org.apache.uima.ruta.engine.RutaTestUtils.TestFeature;
@@ -43,11 +46,11 @@ public class CreateTest {
   public void test() {
     String name = this.getClass().getSimpleName();
     String namespace = this.getClass().getPackage().getName().replaceAll("\\.", "/");
-    
+
     Map<String, String> complexTypes = new TreeMap<String, String>();
     String typeName = "org.apache.uima.C";
     complexTypes.put(typeName, "uima.tcas.Annotation");
-    
+
     Map<String, List<TestFeature>> features = new TreeMap<String, List<TestFeature>>();
     List<TestFeature> list = new ArrayList<RutaTestUtils.TestFeature>();
     features.put(typeName, list);
@@ -57,11 +60,12 @@ public class CreateTest {
     list.add(new TestFeature(fn2, "", "uima.tcas.Annotation"));
     String fn3 = "count";
     list.add(new TestFeature(fn3, "", "uima.cas.Integer"));
-    
+
     CAS cas = null;
     try {
-      cas = RutaTestUtils.process(namespace + "/" + name + RutaEngine.SCRIPT_FILE_EXTENSION, namespace + "/" + name
-              + ".txt", 50, false, false, complexTypes, features, namespace + "/");
+      cas = RutaTestUtils.process(namespace + "/" + name + RutaEngine.SCRIPT_FILE_EXTENSION,
+              namespace + "/" + name + ".txt", 50, false, false, complexTypes, features, namespace
+                      + "/");
     } catch (Exception e) {
       e.printStackTrace();
       assert (false);
@@ -86,7 +90,71 @@ public class CreateTest {
     assertEquals("A", v1.getCoveredText());
     assertEquals("B", v2.getCoveredText());
     assertEquals(2, intValue);
-   
+
     cas.release();
   }
+
+  @Test
+  public void testFeature() {
+    String document = "Test.";
+    String script = "";
+    script += "W{-> CREATE(Inner, \"word\" = W)};";
+    script += "Inner{-> CREATE(Source, \"inner\" = Inner)};";
+    script += "Source{-> CREATE(Target, \"word\" = Source.inner.word)};";
+
+    Map<String, String> typeMap = new TreeMap<String, String>();
+    String typeName1 = "Source";
+    typeMap.put(typeName1, "uima.tcas.Annotation");
+    String typeName2 = "Inner";
+    typeMap.put(typeName2, "uima.tcas.Annotation");
+    String typeName3 = "Target";
+    typeMap.put(typeName3, "uima.tcas.Annotation");
+
+    Map<String, List<TestFeature>> featureMap = new TreeMap<String, List<TestFeature>>();
+    List<TestFeature> list = new ArrayList<RutaTestUtils.TestFeature>();
+    featureMap.put(typeName1, list);
+    String fn1 = "inner";
+    list.add(new TestFeature(fn1, "", typeName2));
+
+    list = new ArrayList<RutaTestUtils.TestFeature>();
+    featureMap.put(typeName2, list);
+
+    String fn2 = "word";
+    list.add(new TestFeature(fn2, "", "uima.tcas.Annotation"));
+
+    list = new ArrayList<RutaTestUtils.TestFeature>();
+    featureMap.put(typeName3, list);
+
+    String fn3 = "word";
+    list.add(new TestFeature(fn3, "", "uima.tcas.Annotation"));
+
+    CAS cas = null;
+    try {
+      cas = RutaTestUtils.getCAS(document, typeMap, featureMap);
+      Ruta.apply(cas, script);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    Type t = null;
+    AnnotationIndex<AnnotationFS> ai = null;
+    FSIterator<AnnotationFS> iterator = null;
+
+    t = cas.getTypeSystem().getType(typeName3);
+    Feature f3 = t.getFeatureByBaseName(fn3);
+    ai = cas.getAnnotationIndex(t);
+    assertEquals(1, ai.size());
+    iterator = ai.iterator();
+    AnnotationFS next = iterator.next();
+    FeatureStructure fv1 = next.getFeatureValue(f3);
+    assertNotNull(fv1);
+
+    assertEquals("Test", ((AnnotationFS) fv1).getCoveredText());
+
+    if (cas != null) {
+      cas.release();
+    }
+
+  }
+
 }
