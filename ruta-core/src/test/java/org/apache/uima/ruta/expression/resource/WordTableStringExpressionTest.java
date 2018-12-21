0685e64811d533b49518ac4878429cdc07702998
Peter Klügl <pkluegl@apache.org>
2018-12-21 14:36:48 +0000

    avoid warning
    git-svn-id: https://svn.apache.org/repos/asf/uima/ruta/trunk@1849482 13f79535-47bb-0310-9956-ffa450edef68

--- ruta-core/src/test/java/org/apache/uima/ruta/expression/resource/WordTableStringExpressionTest.java
+++ ruta-core/src/test/java/org/apache/uima/ruta/expression/resource/WordTableStringExpressionTest.java
@@ -21,7 +21,11 @@ package org.apache.uima.ruta.expression.resource;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.FSIterator;
@@ -37,83 +41,18 @@ import org.junit.Test;
 
 public class WordTableStringExpressionTest {
 
-
   @Test
   public void test() throws Exception {
     String document = "Kluegl Schor Kottmann";
 
-    String script="STRING s = \"org/apache/uima/ruta/action/\";\n";
-    script +="WORDTABLE table = s + \"table.csv\";\n";
-    script +="MARKTABLE(Person, 1, table, true, 0, \"-.,\", 10, \"firstname\" = 2, \"system\" = 3);\n";
-    
+    String script = "STRING s = \"org/apache/uima/ruta/action/\";\n";
+    script += "WORDTABLE table = s + \"table.csv\";\n";
+    script += "MARKTABLE(Person, 1, table, true, 0, \"-.,\", 10, \"firstname\" = 2, \"system\" = 3);\n";
+
     Map<String, String> complexTypes = new TreeMap<String, String>();
     String typeName = "org.apache.uima.Person";
     complexTypes.put(typeName, "uima.tcas.Annotation");
-    
-    Map<String, List<TestFeature>> features = new TreeMap<String, List<TestFeature>>();
-    List<TestFeature> list = new ArrayList<RutaTestUtils.TestFeature>();
-    features.put(typeName, list);
-    String fn1 = "firstname";
-    list.add(new TestFeature(fn1, "", "uima.cas.String"));
-    String fn2 = "system";
-    list.add(new TestFeature(fn2, "", "uima.cas.String"));
 
-    Map<String, Object> map = new HashMap();
-    map.put(RutaEngine.PARAM_DICT_REMOVE_WS, true);
-
-    CAS cas = RutaTestUtils.getCAS(document, complexTypes, features);
-    Ruta.apply(cas, script, map);
-    
-    Type t = null;
-    AnnotationIndex<AnnotationFS> ai = null;
-    FSIterator<AnnotationFS> iterator = null;
-    AnnotationFS next = null;
-    String v1 = null;
-    String v2 = null;
-    t = cas.getTypeSystem().getType(typeName);
-    Feature f1 = t.getFeatureByBaseName(fn1);
-    Feature f2 = t.getFeatureByBaseName(fn2);
-    ai = cas.getAnnotationIndex(t);
-    
-    assertEquals(3, ai.size());
-    iterator = ai.iterator();
-    
-    next = iterator.next();
-    v1 = next.getStringValue(f1);
-    v2 = next.getStringValue(f2);
-    assertEquals("Peter", v1);
-    assertEquals("Ruta", v2);
-    
-    next = iterator.next();
-    v1 = next.getStringValue(f1);
-    v2 = next.getStringValue(f2);
-    assertEquals("Marshall", v1);
-    assertEquals("UIMA", v2);
-    
-    next = iterator.next();
-    v1 = next.getStringValue(f1);
-    v2 = next.getStringValue(f2);
-    assertEquals("Joern", v1);
-    assertEquals("CAS Editor", v2);
-    
-    cas.release();    
-
-  }
-  
-  @Test
-  public void testInBlock() throws Exception {
-    String document = "Kluegl Schor Kottmann";
-
-    String script="STRING s = \"org/apache/uima/ruta/action/\";\n";
-    script +="BLOCK(block) Document{}{\n";
-    script +="WORDTABLE table = s + \"table.csv\";\n";
-    script +="MARKTABLE(Person, 1, table, true, 0, \"-.,\", 10, \"firstname\" = 2, \"system\" = 3);\n";
-    script +="}\n";
-    
-    Map<String, String> complexTypes = new TreeMap<String, String>();
-    String typeName = "org.apache.uima.Person";
-    complexTypes.put(typeName, "uima.tcas.Annotation");
-    
     Map<String, List<TestFeature>> features = new TreeMap<String, List<TestFeature>>();
     List<TestFeature> list = new ArrayList<RutaTestUtils.TestFeature>();
     features.put(typeName, list);
@@ -127,7 +66,7 @@ public class WordTableStringExpressionTest {
 
     CAS cas = RutaTestUtils.getCAS(document, complexTypes, features);
     Ruta.apply(cas, script, map);
-    
+
     Type t = null;
     AnnotationIndex<AnnotationFS> ai = null;
     FSIterator<AnnotationFS> iterator = null;
@@ -138,30 +77,94 @@ public class WordTableStringExpressionTest {
     Feature f1 = t.getFeatureByBaseName(fn1);
     Feature f2 = t.getFeatureByBaseName(fn2);
     ai = cas.getAnnotationIndex(t);
-    
+
     assertEquals(3, ai.size());
     iterator = ai.iterator();
-    
+
     next = iterator.next();
     v1 = next.getStringValue(f1);
     v2 = next.getStringValue(f2);
     assertEquals("Peter", v1);
     assertEquals("Ruta", v2);
-    
+
     next = iterator.next();
     v1 = next.getStringValue(f1);
     v2 = next.getStringValue(f2);
     assertEquals("Marshall", v1);
     assertEquals("UIMA", v2);
-    
+
     next = iterator.next();
     v1 = next.getStringValue(f1);
     v2 = next.getStringValue(f2);
     assertEquals("Joern", v1);
     assertEquals("CAS Editor", v2);
-    
-    cas.release();    
+
+    cas.release();
 
   }
-  
+
+  @Test
+  public void testInBlock() throws Exception {
+    String document = "Kluegl Schor Kottmann";
+
+    String script = "STRING s = \"org/apache/uima/ruta/action/\";\n";
+    script += "BLOCK(block) Document{}{\n";
+    script += "WORDTABLE table = s + \"table.csv\";\n";
+    script += "MARKTABLE(Person, 1, table, true, 0, \"-.,\", 10, \"firstname\" = 2, \"system\" = 3);\n";
+    script += "}\n";
+
+    Map<String, String> complexTypes = new TreeMap<String, String>();
+    String typeName = "org.apache.uima.Person";
+    complexTypes.put(typeName, "uima.tcas.Annotation");
+
+    Map<String, List<TestFeature>> features = new TreeMap<String, List<TestFeature>>();
+    List<TestFeature> list = new ArrayList<RutaTestUtils.TestFeature>();
+    features.put(typeName, list);
+    String fn1 = "firstname";
+    list.add(new TestFeature(fn1, "", "uima.cas.String"));
+    String fn2 = "system";
+    list.add(new TestFeature(fn2, "", "uima.cas.String"));
+
+    Map<String, Object> map = new HashMap<>();
+    map.put(RutaEngine.PARAM_DICT_REMOVE_WS, true);
+
+    CAS cas = RutaTestUtils.getCAS(document, complexTypes, features);
+    Ruta.apply(cas, script, map);
+
+    Type t = null;
+    AnnotationIndex<AnnotationFS> ai = null;
+    FSIterator<AnnotationFS> iterator = null;
+    AnnotationFS next = null;
+    String v1 = null;
+    String v2 = null;
+    t = cas.getTypeSystem().getType(typeName);
+    Feature f1 = t.getFeatureByBaseName(fn1);
+    Feature f2 = t.getFeatureByBaseName(fn2);
+    ai = cas.getAnnotationIndex(t);
+
+    assertEquals(3, ai.size());
+    iterator = ai.iterator();
+
+    next = iterator.next();
+    v1 = next.getStringValue(f1);
+    v2 = next.getStringValue(f2);
+    assertEquals("Peter", v1);
+    assertEquals("Ruta", v2);
+
+    next = iterator.next();
+    v1 = next.getStringValue(f1);
+    v2 = next.getStringValue(f2);
+    assertEquals("Marshall", v1);
+    assertEquals("UIMA", v2);
+
+    next = iterator.next();
+    v1 = next.getStringValue(f1);
+    v2 = next.getStringValue(f2);
+    assertEquals("Joern", v1);
+    assertEquals("CAS Editor", v2);
+
+    cas.release();
+
+  }
+
 }
