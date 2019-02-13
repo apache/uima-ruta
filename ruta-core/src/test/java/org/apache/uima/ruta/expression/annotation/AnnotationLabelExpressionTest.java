dee394f166db5965232d1a6a42ad1dcc98433f91
Peter Klügl <pkluegl@apache.org>
2019-02-13 09:32:08 +0000

    UIMA-5980 - deactivate test file creation
    git-svn-id: https://svn.apache.org/repos/asf/uima/ruta/trunk@1853481 13f79535-47bb-0310-9956-ffa450edef68

--- ruta-core/src/test/java/org/apache/uima/ruta/expression/annotation/AnnotationLabelExpressionTest.java
+++ ruta-core/src/test/java/org/apache/uima/ruta/expression/annotation/AnnotationLabelExpressionTest.java
@@ -657,8 +657,6 @@ public class AnnotationLabelExpressionTest {
     CAS cas = RutaTestUtils.getCAS(document, typeMap, featureMap);
     Ruta.apply(cas, script);
 
-    RutaTestUtils.storeCas(cas, "testStackedReinitLazyFeature");
-
     RutaTestUtils.assertAnnotationsEquals(cas, 1, 1, "Some");
     RutaTestUtils.assertAnnotationsEquals(cas, 2, 1, "Some");
     RutaTestUtils.assertAnnotationsEquals(cas, 3, 1, "Some");
