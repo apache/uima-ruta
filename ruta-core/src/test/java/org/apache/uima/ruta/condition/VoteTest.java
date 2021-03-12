023209d57ba4e2171a657b1b5cfb078c882e0075
Peter Klügl <peter.kluegl@averbis.com>
2021-03-12 10:46:54 +0100

    UIMA-6338: Ruta: Inconsistency Interpreter vs. Guide on VOTE
    - startet to add tests

--- ruta-core/src/test/java/org/apache/uima/ruta/condition/VoteTest.java
+++ ruta-core/src/test/java/org/apache/uima/ruta/condition/VoteTest.java
@@ -20,6 +20,7 @@
 package org.apache.uima.ruta.condition;
 
 import org.apache.uima.cas.CAS;
+import org.apache.uima.ruta.engine.Ruta;
 import org.apache.uima.ruta.engine.RutaTestUtils;
 import org.junit.Test;
 
@@ -30,10 +31,29 @@ public class VoteTest {
 
     CAS cas = RutaTestUtils.processTestScript(this.getClass());
 
-    RutaTestUtils.assertAnnotationsEquals(cas, 1, 1, "Use the VOTE Condition, to compare the number of occurrences"
-            + " of two different annotations.");
+    RutaTestUtils.assertAnnotationsEquals(cas, 1, 1,
+            "Use the VOTE Condition, to compare the number of occurrences"
+                    + " of two different annotations.");
     RutaTestUtils.assertAnnotationsEquals(cas, 2, 0);
-
-    cas.release();
   }
+
+  @Test
+  public void testInWindowOnly() throws Exception {
+
+    String document = "1 2 . A b C d. 3 4";
+
+    String script = "PERIOD #{-> T1} PERIOD;";
+    script += "(CW ANY ANY){->T2};";
+    script += "SW{->T3};";
+    script += "T1{VOTE(T2,T3)->T4};";
+    script += "T1{-VOTE(T2,T3)->T5};";
+
+    CAS cas = RutaTestUtils.getCAS(document);
+    Ruta.apply(cas, script);
+
+    RutaTestUtils.assertAnnotationsEquals(cas, 4, 0);
+    RutaTestUtils.assertAnnotationsEquals(cas, 5, 1, "A b C d");
+
+  }
+
 }
